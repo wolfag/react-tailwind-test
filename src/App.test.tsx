c4f9e827e373a02ce5756f1ee6399be394f8d8d4
TaiNguyen <tai@useget.com>
2021-05-30 21:16:35 +0700

    bk

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App, { TODO_KEY } from './App';
 
-test('renders learn react link', () => {
+test('given saved todos when app is loaded then show those', () => {
+  localStorage.setItem(TODO_KEY, JSON.stringify(['hello', 'youtube']));
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('hello');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('given saved todos when adding another todo then show saved and new todo', () => {
+  localStorage.setItem(TODO_KEY, JSON.stringify(['hello']));
+  render(<App />);
+
+  const inputElement = screen.getByTestId('todo-input');
+  const buttonElement = screen.getByText('Add');
+
+  fireEvent.change(inputElement, { target: { value: 'youtube' } });
+  fireEvent.click(buttonElement);
+
+  const linkElement = screen.getByText('youtube');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('given saved todos when deleting specific todo then remaining todo', () => {
+  localStorage.setItem(TODO_KEY, JSON.stringify(['hello', 'youtube']));
+  render(<App />);
+
+  const deleteIcon = screen.getByTestId('delete-youtube');
+
+  const linkElement = screen.getByText('youtube');
+  fireEvent.click(deleteIcon);
+
+  expect(linkElement).not.toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { AiFillDelete } from 'react-icons/ai';
+
+export const TODO_KEY = 'TODOS';
 
 function App() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const nameRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const data = localStorage.getItem(TODO_KEY);
+    if (data) {
+      setTodos(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = () => {
+    const name = nameRef.current?.value ?? '';
+    if (name === '') return;
+    setTodos([...todos, name]);
+
+    if (nameRef.current) {
+      nameRef.current.value = '';
+    }
+  };
+
+  const removeTodo = (index: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='w-screen h-screen flex flex-col items-center space-y-8 pt-40'>
+      <h1 className='text-4xl font-medium'>Todo list</h1>
+      <div className='flex space-x-2'>
+        <input
+          data-testid='todo-input'
+          type='text'
+          ref={nameRef}
+          className='border rounded py-2 px-4'
+        />
+        <button
+          className='bg-indigo-500 py-2 px-4 rounded text-white hover:bg-indigo-700'
+          onClick={addTodo}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
+      {todos?.map((value, index) => {
+        return (
+          <div key={index} className='flex justify-between w-80'>
+            <p>{value}</p>
+            <div className='text-gray400 cursor-pointer hover:text-gray-700'>
+              <AiFillDelete
+                data-testid={`delete-${value}`}
+                onClick={() => {
+                  removeTodo(index);
+                }}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
